f16501302d7e0901df1ae6e87f40675d573de2d2
purugrammer <147807675+purugrammer@users.noreply.github.com>
2024-06-14 00:07:43 +0530

    Update script.js

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,26 +1,22 @@
 const container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++)
-    {
-        for (let j = 0; j < 16; j++)
-            {
-                const div = document.createElement("divs");
-                div.textContent = "row"
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+                const div = document.createElement("div");
+                div.classList.add("grid-item");
                 container.appendChild(div)
-
             }
-
-            const div = document.createElement("divs");
-            div.textContent = "row"
-            container.appendChild(div)
-
-    }
-
-const divs = document.querySelector("divs");
-divs.addEventListener('click', () => {
-    divs.style.backgroundColor = "black"
+         }
+const divs = document.querySelectorAll(".grid-item");
+divs.forEach(div => {
+    div.addEventListener('mouseover', () => {
+    div.style.backgroundColor = "grey"
     console.log(divs)
-});
+        })
+})
+
+    
+
  
 
 
