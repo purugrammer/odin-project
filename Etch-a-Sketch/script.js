1e5d2a96f399c11bdf5f1d22e2a94a8fdb93f2a9
purugrammer <147807675+purugrammer@users.noreply.github.com>
2024-06-14 14:13:58 +0530

    Update script.js

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,3 +1,5 @@
+
+
 const container = document.querySelector("#container");
 
 for (let i = 0; i < 16; i++) {
@@ -10,13 +12,64 @@ for (let i = 0; i < 16; i++) {
 const divs = document.querySelectorAll(".grid-item");
 divs.forEach(div => {
     div.addEventListener('mouseover', () => {
-    div.style.backgroundColor = "grey"
-    console.log(divs)
+    const randomColor = getRandomColor();
+    div.style.backgroundColor = randomColor
         })
 })
 
+function getRandomColor() {
+const r = Math.floor(Math.random() * 256)
+const g = Math.floor(Math.random() * 256)
+const b = Math.floor(Math.random() * 256)
+return `rgb(${r}, ${g}, ${b})`;
+}
+
+
+const button = document.querySelector(".new-grid");
+
+button.addEventListener('click', () => { createNewGrid() 
+})
+
+function createNewGrid() {
+const container = document.querySelector("#container");
+let num = prompt("Enter the number of Grid Elements (up to 100")
+
+
+num = parseInt(num);
+num = Math.min(num, 100);
+
+    if (isNaN(num) || num < 1) {
+        alert("Invalid input. Please enter a number between 1 and 100.");
+        return; 
+    }
+
+
+    container.innerHTML = "";    
+    container.style.setProperty('--num', num);
+        
+        
+        
+        for (let i = 0; i < num; i++) {
+            for (let j = 0; j < num; j++) {
+                        const div = document.createElement("div");
+                        div.classList.add("new-grid-item");
+                        container.appendChild(div)
+                    }
+                 }
+
+                 const divs2 = document.querySelectorAll(".new-grid-item");
+                 divs2.forEach(div => {
+                 div.addEventListener('mouseover', () => {
+                 const randomColor = getRandomColor();
+                 div.style.backgroundColor = randomColor
+             })
+         })
+
+        }
+        
     
 
- 
 
+
+ 
 
